14078115a05be1f7082e9b948c979d4a7c712023
maithaalhayyas <maithaalhayyas@gmail.com>
2021-03-25 00:45:07 +0400

    list of all students

--- callback_ps.c
+++ callback_ps.c
@@ -11,3 +11,27 @@ struct student
 	int year;
 	int age;
 };
+
+
+struct student class[]=
+{
+	"sean", "penn",2,21,
+	"sean","connery",4,25,
+	"Angelina","jolie";3;32;
+	"meryl","streep",4,29;
+	"Robin","Williams",3,32,
+	"Bill","Gates",3,17,
+	"Jodie","Foster",4,25,
+	"John","Travolta",1,17,
+	"Isaac","Newton",2,19,
+	"Sarah","Palin",2,19,
+};
+
+int compare_fast_name(const void* a, const void* b)
+{
+	struct student* sa=(struct student*)a;
+	struct student* sb=(struct student*)b;
+	return strcmp(sa->lname,sb->lname);
+	
+}
+
