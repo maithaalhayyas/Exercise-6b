4ca7525ca89c0189ac444c8bf9a27004b3261d24
maithaalhayyas <maithaalhayyas@gmail.com>
2021-03-25 00:52:58 +0400

    list of all students

--- callback_ps.c
+++ callback_ps.c
@@ -13,4 +13,19 @@ struct student
 };
 
 
+struct student class[]=
+{
+	"sean", "penn",2,21,
+	"sean","connery",4,25,
+	"Angelina","jolie";3;32;
+	"meryl","streep",4,29;
+	"Robin","Williams",3,32,
+	"Bill","Gates",3,17,
+	"Jodie","Foster",4,25,
+	"John","Travolta",1,17,
+	"Isaac","Newton",2,19,
+	"Sarah","Palin",2,19,
+};
+
+
 
